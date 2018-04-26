21bcd430e2b391b34533b510af42e4c93910ae6c
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-04-26 20:54:00 +0200

    Changed license

--- rewrite-control.php
+++ rewrite-control.php
@@ -2,18 +2,18 @@
 /**
  * Plugin Name: ic Rewrite Control
  * Plugin URI:  https://github.com/inerciacreativa/wp-rewrite-control
- * Version:     1.0.3
+ * Version:     1.0.0
  * Description: Gestor de .htaccess y opciones de WP_Rewrite.
  * Author:      Jose Cuesta
  * Author URI:  https://inerciacreativa.com/
  * Text Domain: ic-rewrite-control
  * Domain Path: /languages
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:     MIT
+ * License URI: https://opensource.org/licenses/MIT
  */
 
 if (!defined('ABSPATH')) {
-    exit;
+	exit;
 }
 
 include_once __DIR__ . '/vendor/autoload.php';
