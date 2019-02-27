74a609d5d3ec467f3f2b12eb920fcadfad35b139
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-02-27 10:24:48 +0100

    Check for requisites

--- rewrite-control.php
+++ rewrite-control.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Rewrite Control
  * Plugin URI:  https://github.com/inerciacreativa/wp-rewrite-control
- * Version:     2.0.7
+ * Version:     2.0.10
  * Text Domain: ic-rewrite-control
  * Domain Path: /languages
  * Description: Gestor de .htaccess y opciones de WP_Rewrite.
@@ -14,6 +14,18 @@
 
 if (!defined('ABSPATH')) {
 	exit;
+}
+
+if (!class_exists(ic\Framework\Framework::class)) {
+	trigger_error('ic Framework not found', E_USER_ERROR);
+}
+
+if (!class_exists(ic\Plugin\RewriteControl\RewriteControl::class)) {
+	if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+		include_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		trigger_error('Could not load RewriteControl class', E_USER_ERROR);
+	}
 }
 
 ic\Plugin\RewriteControl\RewriteControl::create(__FILE__);
