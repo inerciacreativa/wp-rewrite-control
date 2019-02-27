159e2232bab6637b4fc64f2be1da367382bc7986
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-02-27 14:06:52 +0100

    Added method to get common files match pattern

--- source/Apache.php
+++ source/Apache.php
@@ -29,7 +29,7 @@ class Apache
 	/**
 	 * @var array
 	 */
-	protected static $config = [
+	private static $config = [
 		'protect'       => Protect::class,
 		'cors'          => CORS::class,
 		'csp'           => CSP::class,
@@ -47,10 +47,63 @@ class Apache
 		'base'          => Base::class,
 	];
 
+	private static $filesMatchPattern = [
+		'appcache',
+		'atom',
+		'bbaw',
+		'bmp',
+		'br',
+		'crx',
+		'css',
+		'cur',
+		'eot',
+		'f4[abpv]',
+		'flv',
+		'geojson',
+		'gif',
+		'gz',
+		'htc',
+		'ic[os]',
+		'jpe?g',
+		'm?js',
+		'json(ld)?',
+		'm4[av]',
+		'manifest',
+		'map',
+		'markdown',
+		'md',
+		'mp4',
+		'oex',
+		'og[agv]',
+		'opus',
+		'otf',
+		'pdf',
+		'png',
+		'rdf',
+		'rss',
+		'safariextz',
+		'svgz?',
+		'swf',
+		'topojson',
+		'tt[cf]',
+		'txt',
+		'vcard',
+		'vcf',
+		'vtt',
+		'wasm',
+		'webapp',
+		'web[mp]',
+		'webmanifest',
+		'woff2?',
+		'xloc',
+		'xml',
+		'xpi',
+	];
+
 	/**
 	 * @var RewriteControl
 	 */
-	protected $plugin;
+	private $plugin;
 
 	/**
 	 * External constructor.
@@ -63,6 +116,8 @@ class Apache
 	}
 
 	/**
+	 * Generate Apache rules based on current configuration.
+	 *
 	 * @return string
 	 */
 	public function rules(): string
@@ -77,11 +132,21 @@ class Apache
 	}
 
 	/**
-	 *
+	 * Generate .htaccess file.
 	 */
 	public function flush(): void
 	{
 		save_mod_rewrite_rules();
+	}
+
+	/**
+	 * Generate a regular expression for use in several directives.
+	 *
+	 * @return string
+	 */
+	public function getFilesMatchPattern(): string
+	{
+		return '\.(' . implode('|', self::$filesMatchPattern) . ')$';
 	}
 
 }
