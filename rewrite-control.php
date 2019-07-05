ce556734131ed9fbbdfa464e8b0c7469836ef241
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-07-05 05:41:17 +0200

    Updated version

--- rewrite-control.php
+++ rewrite-control.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Rewrite Control
  * Plugin URI:  https://github.com/inerciacreativa/wp-rewrite-control
- * Version:     4.0.1
+ * Version:     4.0.2
  * Text Domain: ic-rewrite-control
  * Domain Path: /languages
  * Description: Gestor de .htaccess y opciones de WP_Rewrite.
